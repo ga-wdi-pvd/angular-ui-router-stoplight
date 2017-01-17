c7c0ee3ad2ae51df771ada2403423cd9b5c19853
Danny Kirschner <rkirschner1377@gmail.com>
2017-01-17 11:54:02 -0500

    Working through code, up until adding partial for stop-light html code

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 (function(){
 angular
-  .module("stopLightApp", [])
-  .controller("stopLightController", [stopLightController])
+  .module("stopLightApp", ["ui.router"])
+  .config(["$stateProvider", Router])
+  .controller("stopLightController", ["$state", "$stateParams", stopLightController])
 
+  function Router($stateProvider){
+    $stateProvider
+    .state("color",{
+      url: "/:color",
+      controller: "stopLightController",
+      controllerAs: "vm"
+    });
+    console.log("router set up correctly")
+  }
 
-function stopLightController(){
+function stopLightController($state, $stateParams){
+  console.log($stateParams)
+  this.bg = $stateParams.color
   this.colors = ['red', 'yellow','green']
   this.turn = function(color){
+    // within click function
+    $state.go("color", {color: color})
     this.bg = color
   }
 }
